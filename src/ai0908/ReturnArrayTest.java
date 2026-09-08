e84a643471c97924d0ad8b8c5fca7475f587af7a
psy0635-ctrl <psy0635@gmail.com>
2026-09-08 16:28:17 +0900

    제 10장 메소ë,매개변수,반환,오버로딩,배열객체 참ì조값 받아서 출력

---
+++ src/ai0908/ReturnArrayTest.java
@@ -0,0 +1,26 @@
+package ai0908;
+
+public class ReturnArrayTest {
+    public static int[] resultCalc(int n1, int n2){
+        int[] resultArr =  new int[4];
+        resultArr[0] = n1 + n2;
+        resultArr[1] = n1 - n2;
+        resultArr[2] = n1 * n2;
+        resultArr[3] = n1 / n2;
+
+
+        return resultArr;
+    }
+
+    public static void main(String[] args) {
+        int n1 = 200;
+        int n2 = 500;
+
+        int[] resultArr = resultCalc(n1,n2);
+        char[] op = {'+','-','*','/'};
+
+        for(int i=0;i<op.length;i++){
+            System.out.printf("%d %c %d = %d\n",n1,op[i],n2,resultArr[i]);
+        }
+    }
+}
